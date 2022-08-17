1811f19408a651fa7fba2010cc4e5b4e650cf747
hdmun <hdmun@kakao.com>
2022-08-17 18:09:22 +0900

    [frontend] 토너먼트 시계 UI 프로토 타이핑
    - `nuxtjs/storybook`을 적용해보았다.

---
+++ poker-time-table-frontend/components/tournamentClock.stories.ts
@@ -0,0 +1,10 @@
+import { VApp } from 'vuetify/lib'
+
+export default {
+  title: 'TournamentClock'
+}
+
+export const TournamentClock = () => ({
+  template: '<v-app><TournamentClock /></v-app>',
+  components: { VApp }
+})
--- poker-time-table-frontend/components/tournamentClock.vue
+++ poker-time-table-frontend/components/tournamentClock.vue
@@ -1,36 +1,80 @@
 <template>
-  <v-container>
-    <v-card>
-      <v-list-item two-line>
-        <v-list-item-content>
-          <v-list-item-title class="text-h5 text-center">
-            Level {{ level }}
-          </v-list-item-title>
-          <v-list-item-subtitle class="text-center">{{ title }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+  <v-card height="100%">
+    <v-row justify="center">
+      <v-col lg="2">
+        <v-card-title class="justify-center">
+          PLAY TIME
+        </v-card-title>
 
-      <v-card-text>
-        <v-row align="center">
-          <v-col class="text-h2 text-center">
-            {{ remainTime }}
-          </v-col>
-        </v-row>
-        <v-row align="center">
-          <v-col class="text-h4 text-center">
-            {{ smallBlind }} / {{ bigBlind }}
-          </v-col>
-        </v-row>
-      </v-card-text>
+        <v-card-title class="justify-center pt-0">
+          {{ playTime }}
+        </v-card-title>
+      </v-col>
 
-      <v-list-item>
-        <v-list-item-title class="text-center">
-          Next Blind: {{ nextSmallBlind }} / {{ nextBigBlind }}
+      <v-col lg="2">
+        <v-card-title class="justify-center">
+          NEXT BREAK
+        </v-card-title>
+
+        <v-card-title class="justify-center pt-0">
+          {{ nextBreakTime }}
+        </v-card-title>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-0">
+      <v-col>
+        <v-card-title class="justify-center" style="font-size: 12em; line-height: 12rem;">
+          {{ remainTime }}
+        </v-card-title>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center" class="mb-6">
+      <v-btn tile outlined class="ma-2">
+        <v-icon>
+          mdi-chevron-left
+        </v-icon>
+      </v-btn>
+      <v-btn tile outlined class="ma-2">
+        <v-icon v-if="playing">
+          mdi-pause
+        </v-icon>
+        <v-icon v-else>
+          mdi-play
+        </v-icon>
+      </v-btn>
+      <v-btn tile outlined class="ma-2">
+        <v-icon>
+          mdi-chevron-right
+        </v-icon>
+      </v-btn>
+    </v-row>
+
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 text-center">
+          Level {{ level }}
         </v-list-item-title>
-      </v-list-item>
+        <v-list-item-subtitle class="text-center">{{ title }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
 
-    </v-card>
-  </v-container>
+    <v-card-text>
+      <v-row justify="center">
+        <v-col class="text-h4 text-center">
+          {{ smallBlind }} / {{ bigBlind }}
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <v-list-item>
+      <v-list-item-title class="text-center">
+        Next Blind: {{ nextSmallBlind }} / {{ nextBigBlind }}
+      </v-list-item-title>
+    </v-list-item>
+
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -52,6 +96,9 @@ import dummyData from '~/data/tournamentClockDto'
 export default class TournamentClock extends Vue {
   level: number = 1
   title: string = ''
+
+  playTime: string = '03:27:04'
+  nextBreakTime: string = '58:42'
   remainTime: string = '00:00'
 
   timer: number = 0
@@ -60,6 +107,8 @@ export default class TournamentClock extends Vue {
   bigBlind: number = 0
   nextSmallBlind: number = 0
   nextBigBlind: number = 0
+
+  playing: boolean = false
 
   created() { }
 
