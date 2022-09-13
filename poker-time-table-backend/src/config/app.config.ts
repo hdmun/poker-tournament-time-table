d948fff7cc1c0ee5f973c2e301218e9201cf7260
hdmun <hdmun@kakao.com>
2022-09-13 18:12:49 +0900

    [backend] `.env.test` 제거
    - `ignoreEnvFile` 임시 주석 처리

--- poker-time-table-backend/src/config/app.config.ts
+++ poker-time-table-backend/src/config/app.config.ts
@@ -3,10 +3,10 @@ import * as Joi from 'joi';
 
 export const AppConfigMoudle = ConfigModule.forRoot({
   isGlobal: true,
-  envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-  ignoreEnvFile: process.env.NODE_ENV === 'prod', // prod할 때는 heroku에 따로 넣기로
+  envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+  // ignoreEnvFile: process.env.NODE_ENV === 'prod', // prod할 때는 heroku에 따로 넣기로
   validationSchema: Joi.object({
-    NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+    NODE_ENV: Joi.string().valid('dev', 'prod').required(),
     DB_HOST: Joi.string().required(),
     DB_PORT: Joi.string().required(),
     DB_USER: Joi.string().required(),
