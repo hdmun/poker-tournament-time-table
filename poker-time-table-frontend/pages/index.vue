f9de89e02761318c59928ccfafb4f22623f31acb
hdmun <huidae.mun@gmail.com>
2022-09-13 11:56:21 +0900

    [frontend] 메인 화면 사이즈 반응형 처리
    - 사이드 메뉴 텍스트 변경

--- poker-time-table-frontend/pages/index.vue
+++ poker-time-table-frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-flex>
     <v-row class="pa-6">
-      <v-col lg="6">
+      <v-col cols="12" sm="12" md="6" lg="6" xl="6">
         <TournamentTable ref="tournamentTable" />
       </v-col>
       <v-col lg="9" class="pa-0"> </v-col>
