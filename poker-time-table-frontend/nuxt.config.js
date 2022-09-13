ddeae7814775d39c9a9a546b282e9cd0701f9eab
hdmun <hdmun@kakao.com>
2022-09-13 18:11:33 +0900

    [frontend] nuxt.config.js `dev` 플래그 세팅

--- poker-time-table-frontend/nuxt.config.js
+++ poker-time-table-frontend/nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  dev: process.env.NODE_ENV !== 'production',
   ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
